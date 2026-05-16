0231f1e24e504696e7880dba7a84a38b89315a64
Ved Narvekar <175046711+vednarvekar001@users.noreply.github.com>
2026-05-17 00:20:03 +0530

    added docker

--- models/cnn/train/model.py
+++ models/cnn/train/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CNNBranch(nn.Module):
-    def __init__(self):
+    def __init__(self, num_features):
         super().__init__()
         # Input shape: (Batch, 1, 60, 6)
         self.conv1 = nn.Conv2d(1, 32, kernel_size=(3,2), padding=1)
